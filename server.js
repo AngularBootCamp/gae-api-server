08e28631ce6d374b2acb262cdaf59e4d30d2b3c6
Kyle Cordes <kyle.cordes@oasisdigital.com>
2018-02-27 11:36:37 -0600

    Start work on graphql endpoint

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 const jsonServer = require('json-server');
+const jsonGraphqlExpress = require('json-graphql-server').default;
+const fs = require('fs');
+
+const data = JSON.parse(fs.readFileSync('data.json'));
 
 const server = jsonServer.create();
-const router = jsonServer.router('data.json');
+const router = jsonServer.router(data);
 const middlewares = jsonServer.defaults({
     readOnly: true
 });
@@ -10,6 +14,22 @@ const PORT = process.env.PORT | 3000;
 
 server.use(middlewares);
 server.use(router);
+
+server.use('/graphql', jsonGraphqlExpress({
+    posts: [
+        { id: 1, title: "Lorem Ipsum", views: 254, user_id: 123 },
+        { id: 2, title: "Sic Dolor amet", views: 65, user_id: 456 },
+    ],
+    users: [
+        { id: 123, name: "John Doe" },
+        { id: 456, name: "Jane Doe" }
+    ],
+    comments: [
+        { id: 987, post_id: 1, body: "Consectetur adipiscing elit", date: new Date('2017-07-03') },
+        { id: 995, post_id: 1, body: "Nam molestie pellentesque dui", date: new Date('2017-08-17') }
+    ]
+}));
+
 server.listen(PORT, () => {
-    console.log('JSON Server is running')
+    console.log('JSON Server + GraphQL JSON Server is running')
 });
